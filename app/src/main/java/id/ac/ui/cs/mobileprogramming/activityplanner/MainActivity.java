7d58c0dcc0baef0d4782267299f4aac4bbf2b237
addffa <adli.daffa5@gmail.com>
2020-10-03 14:30:51 +0700

    override back button

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/activityplanner/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/activityplanner/MainActivity.java
@@ -1,13 +1,14 @@
 package id.ac.ui.cs.mobileprogramming.activityplanner;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import id.ac.ui.cs.mobileprogramming.activityplanner.R;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "MESSAGE";
@@ -20,9 +21,25 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit")
+                .setMessage("Are you sure want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
